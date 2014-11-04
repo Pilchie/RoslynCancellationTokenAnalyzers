d3b14a336daa71713351ae141020eda7837e237b
Kevin Pilch-Bisson <kevinpi@microsoft.com>
2014-11-04 09:10:01 -0800

    Fix some typos

--- CancellationAnalyzer/CancellationAnalyzer.Test/Helpers/DiagnosticVerifier.Helper.cs
+++ CancellationAnalyzer/CancellationAnalyzer.Test/Helpers/DiagnosticVerifier.Helper.cs
@@ -34,9 +34,9 @@ namespace TestHelper
         /// Given classes in the form of strings, their language, and an IDiagnosticAnlayzer to apply to it, return the diagnostics found in the string after converting it to a document.
         /// </summary>
         /// <param name="sources">Classes in the form of strings</param>
-        /// <param name="language">The language the soruce classes are in</param>
+        /// <param name="language">The language the source classes are in</param>
         /// <param name="analyzer">The analyzer to be run on the sources</param>
-        /// <returns>An IEnumerable of Diagnostics that surfaced in teh source code, sorted by Location</returns>
+        /// <returns>An IEnumerable of Diagnostics that surfaced in the source code, sorted by Location</returns>
         private static Diagnostic[] GetSortedDiagnostics(string[] sources, string language, DiagnosticAnalyzer analyzer)
         {
             return GetSortedDiagnosticsFromDocuments(analyzer, GetDocuments(sources, language));
@@ -49,7 +49,7 @@ namespace TestHelper
         /// <param name="analyzer">The analyzer to run on the documents</param>
         /// <param name="documents">The Documents that the analyzer will be run on</param>
         /// <param name="spans">Optional TextSpan indicating where a Diagnostic will be found</param>
-        /// <returns>An IEnumerable of Diagnostics that surfaced in teh source code, sorted by Location</returns>
+        /// <returns>An IEnumerable of Diagnostics that surfaced in the source code, sorted by Location</returns>
         protected static Diagnostic[] GetSortedDiagnosticsFromDocuments(DiagnosticAnalyzer analyzer, Document[] documents)
         {
             var projects = new HashSet<Project>();
@@ -92,7 +92,7 @@ namespace TestHelper
         }
 
         /// <summary>
-        /// Sort diagnostices by location in source document
+        /// Sort diagnostics by location in source document
         /// </summary>
         /// <param name="diagnostics">The list of Diagnostics to be sorted</param>
         /// <returns>An IEnumerable containing the Diagnostics in order of Location</returns>
@@ -105,11 +105,11 @@ namespace TestHelper
 
         #region Set up compilation and documents
         /// <summary>
-        /// Given an array of strings as soruces and a language, turn them into a project and return the documents and spans of it.
+        /// Given an array of strings as sources and a language, turn them into a <see cref="Project"/> and return the documents and spans of it.
         /// </summary>
         /// <param name="sources">Classes in the form of strings</param>
         /// <param name="language">The language the source code is in</param>
-        /// <returns>A Tuple containing the Documents produced from the sources and thier TextSpans if relevant</returns>
+        /// <returns>A Tuple containing the Documents produced from the sources and their TextSpans if relevant</returns>
         private static Document[] GetDocuments(string[] sources, string language)
         {
             if (language != LanguageNames.CSharp && language != LanguageNames.VisualBasic)
@@ -134,11 +134,11 @@ namespace TestHelper
         }
 
         /// <summary>
-        /// Create a Document from a string through creating a project that contains it.
+        /// Create a <see cref="Document "/> from a string through creating a project that contains it.
         /// </summary>
         /// <param name="source">Classes in the form of a string</param>
         /// <param name="language">The language the source code is in</param>
-        /// <returns>A Document created from the source string</returns>
+        /// <returns>A <see cref="Document"/> created from the source string</returns>
         protected static Document CreateDocument(string source, string language = LanguageNames.CSharp)
         {
             return CreateProject(new[] { source }, language).Documents.First();
@@ -149,7 +149,7 @@ namespace TestHelper
         /// </summary>
         /// <param name="sources">Classes in the form of strings</param>
         /// <param name="language">The language the source code is in</param>
-        /// <returns>A Project created out of the Douments created from the source strings</returns>
+        /// <returns>A <see cref="Project"/> created out of the <see cref="Document"/>s created from the source strings</returns>
         private static Project CreateProject(string[] sources, string language = LanguageNames.CSharp)
         {
             string fileNamePrefix = DefaultFilePathPrefix;
