dbd55c3e53121ab0f1ac9480b98e84937da926e0
Kevin Pilch-Bisson <kevinpi@microsoft.com>
2014-11-05 17:53:01 -0800

    Add a rule for things not named cancellationToken

---
+++ CancellationAnalyzer/CancellationAnalyzer/CancellationTokenMustBeNamedCancellationToken.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CancellationAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class CancellationTokenMustBeNamedCancellationTokenAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "CT1004";
+        public const string MessageFormat = "Parameter '{0}' should be named 'cancellationToken'";
+        internal const string Title = "CancellationToken parameters should be named 'cancellationToken'";
+        internal const string Category = "ApiDesign";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterCompilationStartAction(compilationContext =>
+            {
+                var cancellationTokenType = compilationContext.Compilation.GetTypeByMetadataName("System.Threading.CancellationToken");
+                if (cancellationTokenType != null)
+                {
+                    compilationContext.RegisterSymbolAction(symbolContext =>
+                    {
+                        var methodSymbol = (IMethodSymbol)symbolContext.Symbol;
+                        foreach (var parameter in methodSymbol.Parameters)
+                        {
+                            if (parameter.Type.Equals(cancellationTokenType)
+                                && parameter.Name != "cancellationToken")
+                            {
+                                symbolContext.ReportDiagnostic(Diagnostic.Create(
+                                    Rule, parameter.Locations.First(), parameter.Name));
+                            }
+                        }
+                    },
+                    SymbolKind.Method);
+                }
+            });
+        }
+    }
+}
