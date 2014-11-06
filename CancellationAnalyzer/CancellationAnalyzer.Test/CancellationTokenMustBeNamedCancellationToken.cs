8d7bcb242797ac08fdf0521c39e96724aec0a827
Kevin Pilch-Bisson <kevinpi@microsoft.com>
2014-11-05 17:53:01 -0800

    Add a rule for things not named cancellationToken

---
+++ CancellationAnalyzer/CancellationAnalyzer.Test/CancellationTokenMustBeNamedCancellationToken.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestHelper;
+using CancellationAnalyzer;
+
+namespace CancellationAnalyzer.Test
+{
+    [TestClass]
+    public class CancellationTokenMustBeNamedCancellationToken : CodeFixVerifier
+    {
+        [TestMethod]
+        public void NoDiagnosticInWhenNamedCorrectly()
+        {
+            var test = @"
+using System.Threading;
+class C
+{
+    void M(CancellationToken cancellationToken) { }
+}";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void DiagnosticWhenNamedIncorrectly()
+        {
+            var source = @"
+using System.Threading;
+class C
+{
+    void M(CancellationToken ct) { }
+}";
+
+            var expected = new DiagnosticResult
+            {
+                Id = CancellationTokenMustBeNamedCancellationTokenAnalyzer.DiagnosticId,
+                Message = String.Format(CancellationTokenMustBeNamedCancellationTokenAnalyzer.MessageFormat, "ct"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 5, 30)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(source, expected);
+
+            var fixedSource = @"
+using System.Threading;
+class C
+{
+    void M(CancellationToken cancellationToken) { }
+}";
+
+            //VerifyCSharpFix(source, fixedSource);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return null/*new CancellationTokenMustBeNamedCancellationTokenCodeFixProvider()*/;
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new CancellationTokenMustBeNamedCancellationTokenAnalyzer();
+        }
+    }
+}
